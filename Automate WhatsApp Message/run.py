a7583ca54c62e677456568768bffb10520a5db87
Raghavendar Cherlagudem <raghuraghavendar9999@gmail.com>
2021-01-25 21:48:44 +0530

    Add files via upload

---
+++ Automate WhatsApp Message/run.py
@@ -0,0 +1,8 @@
+'''
+INSTALL A PYTHON  PYWHATKIT LIBRARY IN YOUR TERMINAL OF YOUR IDE
+PYWHATKIT:pywhatkit is a Python library for sending whatsapp message at certain time, it has several other features too.
+'''
+import pywhatkit
+
+
+pywhatkit.sendwhatmsg('+916301924162','Automated WhatsApp message',21,17)
